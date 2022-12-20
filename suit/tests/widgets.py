458696590983d48b6f857397bbefd6ab41bbc022
Gustavo Lepri <gustavolepri@gmail.com>
2022-12-20 08:27:46 -0300

    django 4.1.4 compatibility

--- suit/tests/widgets.py
+++ suit/tests/widgets.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from suit.widgets import LinkedSelect, HTML5Input, EnclosedInput, \
     NumberInput, SuitDateWidget, SuitTimeWidget, SuitSplitDateTimeWidget, \
     AutosizedTextarea
-from django.utils.translation import ugettext as _
-from django.contrib.admin.templatetags.admin_static import static
+from django.utils.translation import gettext as _
+from django.contrib.staticfiles.templatetags.staticfiles import static
 from suit import utils
 
 django_version = utils.django_major_version()
